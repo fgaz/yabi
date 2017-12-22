a574cc28eb8bc697d5107030dd9c06b806b264a3
Francesco Gazzetta <francygazz@gmail.com>
2017-12-22 11:43:03 +0100

    Somewhat better indentation

--- yabi.hs
+++ yabi.hs
@@ -32,10 +32,15 @@ defaultMemory = (repeat 0, repeat 0)
 --an open bracket adds 1 to the balance, a closed one subtracts 1
 breakWhenBalanced :: Int -> String -> (String, String)
 breakWhenBalanced 0 str = ("",str)
-breakWhenBalanced balance (s:str) | s == '[' = let (first, second) = breakWhenBalanced (balance+1) str in (s:first, second)
-                                  | s == ']' = let (first, second) = breakWhenBalanced (balance-1) str in (s:first, second)
-                                  | otherwise = let (first, second) = breakWhenBalanced balance str in (s:first, second)
-breakWhenBalanced balance [] = error $ "malformed code, brackets balance off by " ++ show balance
+breakWhenBalanced balance (s:str)
+  | s == '['  = let (first, second) = breakWhenBalanced (balance+1) str
+                in (s:first, second)
+  | s == ']'  = let (first, second) = breakWhenBalanced (balance-1) str
+                in (s:first, second)
+  | otherwise = let (first, second) = breakWhenBalanced balance str
+                in (s:first, second)
+breakWhenBalanced balance [] =
+  error $ "malformed code, brackets balance off by " ++ show balance
 
 
 --parse a string to a Program
@@ -43,12 +48,13 @@ bfParse :: String -> Program -> Program
 bfParse "" prog = prog --edge condition
 --adds a Loop containing the code inside the brackets
 bfParse ('[':str) prog = bfParse str' $ prog ++ [Loop loop]
-    where loop = bfParse (init loopStr) []
-          (loopStr, str') = breakWhenBalanced 1 str
+  where loop = bfParse (init loopStr) []
+        (loopStr, str') = breakWhenBalanced 1 str
 --all other instructions
 bfParse (char:str) prog = bfParse str $ prog ++ instruction
-    where instruction = case char2instruction char of Just a -> [a]
-                                                      Nothing -> [] --comments
+  where instruction = case char2instruction char
+                      of Just a -> [a]
+                         Nothing -> [] --comments
 
 char2instruction :: Char -> Maybe Instruction
 char2instruction '>' = Just Next
@@ -72,30 +78,38 @@ bf (Plus:commands) (ml, m:mr) = bf commands (ml, (m+1):mr)
 bf (Minus:commands) (ml, m:mr) = bf commands (ml, (m-1):mr)
 --output of pointed byte
 bf (Output:commands) (ml, m:mr) = do
-                                    putChar $ chr $ fromIntegral m
-                                    bf commands (ml, m:mr)
+  putChar $ chr $ fromIntegral m
+  bf commands (ml, m:mr)
 --input to pointed byte
 bf (Input:commands) (ml, _:mr) = do
-                                   char <- getChar
-                                   let m = fromIntegral $ ord char
-                                   bf commands (ml, m:mr)
+  char <- getChar
+  let m = fromIntegral $ ord char
+  bf commands (ml, m:mr)
 --loop (brackets)
-bf ((Loop loop):commands) (ml, m:mr) | m == 0 = bf commands (ml, m:mr) --skip the loop
-                                     | otherwise = do
-                                                     memory' <- bf loop (ml, m:mr) --execute the loop one time
-                                                     bf ((Loop loop):commands) memory' --recurse
+bf ((Loop loop):commands) (ml, m:mr)
+  | m == 0 = bf commands (ml, m:mr) --skip the loop
+  | otherwise = do
+    memory' <- bf loop (ml, m:mr) --execute the loop one time
+    bf ((Loop loop):commands) memory' --recurse
 
 --debug
 --memory dump ('#', according to Urban Müller's original interpreter)
 bf (MDump:commands) (ml,m:mr) = do
-                                  hPutStrLn stderr "Memory dump:"
-                                  hPutStrLn stderr $ "  " ++ show (takeWhile (/=0) ml) ++ " >" ++ show m ++ "< " ++ show (takeWhile (/=0) mr)
-                                  bf commands (ml,m:mr)
+  hPutStrLn stderr "Memory dump:"
+  -- This is not ideal. To do this correctly we should keep track of what
+  -- was evaluated so far
+  hPutStrLn stderr $ "  "
+                  ++ show (takeWhile (/=0) ml)
+                  ++ " >"
+                  ++ show m
+                  ++ "< "
+                  ++ show (takeWhile (/=0) mr)
+  bf commands (ml,m:mr)
 --program dump
 bf (PDump:commands) memory = do
-                               hPutStrLn stderr "Program dump:"
-                               hPutStrLn stderr $ "  " ++ show commands
-                               bf commands memory
+  hPutStrLn stderr "Program dump:"
+  hPutStrLn stderr $ "  " ++ show commands
+  bf commands memory
 --catch-all pattern. I have yet to discover a way to fall down there
 --whitout the infinite zipper it would be out of memory
 bf _ _ = error "wtf error."
@@ -103,20 +117,20 @@ bf _ _ = error "wtf error."
 
 main :: IO ()
 main = do
-    args <- getArgs
-    when (length args /= 1) $ do
-        putStrLn "Usage: yabi path"
-        exitFailure
-    rawProgram <- readFile $ head args
-    hPutStrLn stderr "[yabi] Parsing..."
-    --parse the program string
-    let program = bfParse rawProgram []
-    hPutStrLn stderr "[yabi] Parsed. Executing..."
-    --disable buffering
-    hSetBuffering stdout NoBuffering
-    hSetBuffering stdin NoBuffering
-    --start the interpreter
-    --we need the side effects, not the final memory, hence `_ <-`
-    _ <- bf program defaultMemory
-    return ()
+  args <- getArgs
+  when (length args /= 1) $ do
+    putStrLn "Usage: yabi path"
+    exitFailure
+  rawProgram <- readFile $ head args
+  hPutStrLn stderr "[yabi] Parsing..."
+  --parse the program string
+  let program = bfParse rawProgram []
+  hPutStrLn stderr "[yabi] Parsed. Executing..."
+  --disable buffering
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stdin NoBuffering
+  --start the interpreter
+  --we need the side effects, not the final memory, hence `_ <-`
+  _ <- bf program defaultMemory
+  return ()
 
